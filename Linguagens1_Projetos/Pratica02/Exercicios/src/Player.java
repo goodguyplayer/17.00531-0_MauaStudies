096e57d80d488de7be6714d55b4050b56ddf6567
NathanBSilva <17.00531-0@maua.br>
2020-03-09 19:12:38 -0300

    Exercise 2 - Part 2 done. I  guess? It only asks for a player class with name and play, so uh... It's done.

---
+++ Linguagens1_Projetos/Pratica02/Exercicios/src/Player.java
@@ -0,0 +1,5 @@
+public class Player {
+    // Part 2.
+    public String name;
+    public RockPaperScissors play;
+}
--- Linguagens1_Projetos/Pratica02/Exercicios/src/RockPaperScissors.java
+++ Linguagens1_Projetos/Pratica02/Exercicios/src/RockPaperScissors.java
@@ -1,6 +1,6 @@
 public class RockPaperScissors {
     // Let me just say I do not like this.
     // This class gets the int. Just it. Win tests comes from part 3 and 4
-    // Paper - 1. Scissors - 2. Rock - 3.
-    int playerinput;
+    // Paper - 1. Scissors - 2. Rock - 3. Or do it purely by text.
+    public String playerinput;
 }
