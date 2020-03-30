49f917a23a5d94dc81d648cf204cee47106f2049
NathanBSilva <17.00531-0@maua.br>
2020-03-30 10:07:53 -0300

    Learned statics Remind me to organize the folders later

---
+++ Linguagens1_Projetos/src/Pratica04/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        MinhaClasse m1 = new MinhaClasse();
+        MinhaClasse m2 = new MinhaClasse();
+        System.out.println("Total objetos.: " + m1.getContaObjetos());
+        //System.out.println();
+    }
+}
---
+++ Linguagens1_Projetos/src/Pratica04/src/MinhaClasse.java
@@ -0,0 +1,13 @@
+public class MinhaClasse {
+    private int ContaObjetos = 0;
+
+    public MinhaClasse(){
+        this.ContaObjetos = this.ContaObjetos + 1;
+    }
+
+    public int getContaObjetos(){
+        return this.ContaObjetos;
+
+    }
+
+}
