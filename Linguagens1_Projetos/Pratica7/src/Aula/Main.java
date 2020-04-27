4051c970c5815d84692695c7ce420f895be8ddf4
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-04-27 10:40:06 -0300

    Learned for each (java)
    For someone who loves working with arrays, this is great

--- Linguagens1_Projetos/Pratica7/src/Aula/Main.java
+++ Linguagens1_Projetos/Pratica7/src/Aula/Main.java
@@ -1,7 +1,31 @@
 package Aula;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+        Lobo lobo = new Lobo();
+        Peixe peixe = new Peixe();
 
+        ArrayList<Animal> animals = new ArrayList();
+        animals.add(cachorro);
+        animals.add(lobo);
+        animals.add(peixe);
+
+        // Method 1
+        for (int i = 0; i< animals.size(); i++){
+            animals.get(i).comer();
+        }
+
+        // Method 2 (Enhanced for)
+        // For (Class i (in): array)
+        for (Animal animal: animals) {
+            animal.comer();
+        }
+
+//        cachorro.comer();
+//        lobo.comer();
+//        peixe.comer();
     }
 }
