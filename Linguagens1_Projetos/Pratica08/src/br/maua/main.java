f72f6b1716111c1b60a02758ab3200ec6c190ed7
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-05-04 09:59:28 -0300

    Learned Import
    Gonna be fun

---
+++ Linguagens1_Projetos/Pratica08/src/br/maua/main.java
@@ -0,0 +1,10 @@
+package br.maua;
+//import pacote.QualquerCoisa;
+
+public class main {
+    public static void main(String[] args) {
+        pacote.QualquerCoisa meuQualquerCoisa = new pacote.QualquerCoisa();
+        //QualquerCoisa meuQualquerCoisa = new QualquerCoisa(); // If import pacote.QualquerCoisa was written before
+        meuQualquerCoisa.hello();
+    }
+}
---
+++ Linguagens1_Projetos/Pratica08/src/pacote/QualquerCoisa.java
@@ -0,0 +1,7 @@
+package pacote;
+
+public class QualquerCoisa {
+    public void hello(){
+        System.out.println("Hello World");
+    }
+}
