d9c601f96bffb5a2e22cdd0978e51e0483745927
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-05-04 11:03:51 -0300

    First half of 0504 class

---
+++ Linguagens1_Projetos/Pratica0802/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("inicio main");
+        metodo1();
+        System.out.println("Fim main");
+    }
+
+    static void metodo1(){
+        System.out.println("inicio metodo1");
+        metodo2();
+        System.out.println("Fim metodo 2");
+    }
+
+    static void metodo2(){
+        System.out.println("inicio metodo 2");
+        int[] array = new int[10];
+        for (int i= 0; i<= 15; i++){
+            array[i] = i;
+            System.out.println(i);
+        }
+        System.out.println("Fim metodo 2");
+    }
+}
