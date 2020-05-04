d5129d9a177bfe56e91f7ce187dad14589b7a881
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-05-04 10:26:38 -0300

    Moving to Exceptions

--- Linguagens1_Projetos/Pratica08/src/br/maua/main.java
+++ Linguagens1_Projetos/Pratica08/src/br/maua/main.java
@@ -1,10 +1,19 @@
 package br.maua;
 //import pacote.QualquerCoisa;
 
+import pacote.QualquerCoisa;
+import pacote.Dog;
+// import pacote.*; //Geral/Import all
+
 public class main {
     public static void main(String[] args) {
-        pacote.QualquerCoisa meuQualquerCoisa = new pacote.QualquerCoisa();
-        //QualquerCoisa meuQualquerCoisa = new QualquerCoisa(); // If import pacote.QualquerCoisa was written before
+        QualquerCoisa meuQualquerCoisa = new QualquerCoisa();
+        Dog doggy = new Dog(30);
+        Dog doggo = new Dog(50);
+        doggy.bark();
         meuQualquerCoisa.hello();
+        doggo.bark();
+        //QualquerCoisa meuQualquerCoisa = new QualquerCoisa(); // If import pacote.QualquerCoisa was written before
+
     }
 }
