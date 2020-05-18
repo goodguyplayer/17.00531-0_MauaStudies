e64bfba5d439a28cc4d1ef5ef477a2519af7914d
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-05-18 10:17:55 -0300

    Started Class - 10

---
+++ Linguagens1_Projetos/Pratica10/src/com/company/Game/PicturePoker.java
@@ -0,0 +1,7 @@
+package com.company.Game;
+
+public class PicturePoker {
+    public static void run(){
+        System.out.println("Mario poker");
+    }
+}
---
+++ Linguagens1_Projetos/Pratica10/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.company.Game.PicturePoker;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PicturePoker.run();
+	// write your code here
+    }
+}
