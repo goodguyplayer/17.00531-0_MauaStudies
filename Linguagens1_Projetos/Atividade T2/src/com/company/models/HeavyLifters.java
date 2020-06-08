1a4254e083c9a574006801cba4fa151e55f5e5f6
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-06-08 12:16:13 -0300

    MobileMembers update

--- Linguagens1_Projetos/Atividade T2/src/com/company/models/HeavyLifters.java
+++ Linguagens1_Projetos/Atividade T2/src/com/company/models/HeavyLifters.java
@@ -1,4 +1,22 @@
 package com.company.models;
 
-public class HeavyLifters {
+import com.company.enums.Horarios;
+import com.company.interfaces.PostarMensagem;
+
+public class HeavyLifters  implements PostarMensagem {
+    private String mensagem;
+
+    @Override
+    public String postarMensagem(Horarios horarios) {
+
+        switch (horarios){
+            case REGULAR:
+                this.mensagem = "- Podem contar conosco!";
+                break;
+            case EXTRA:
+                this.mensagem =  "- N00b_qu3_n_Se_r3pita.bat";
+                break;
+        }
+        return this.mensagem;
+    }
 }
--- Linguagens1_Projetos/Atividade T2/src/com/company/models/MobileMembers.java
+++ Linguagens1_Projetos/Atividade T2/src/com/company/models/MobileMembers.java
@@ -5,19 +5,19 @@ import com.company.enums.Membro;
 import com.company.interfaces.PostarMensagem;
 
 public class MobileMembers implements PostarMensagem {
-    public MobileMembers() {
-    }
-
+private String mensagem;
 
     @Override
     public String postarMensagem(Horarios horarios) {
+
         switch (horarios){
             case REGULAR:
-                return "- Happy Coding!";
+                this.mensagem = "- Happy Coding!";
                 break;
             case EXTRA:
-                return  "- Happy_C0d1ng. Maskers";
+                this.mensagem =  "- Happy_C0d1ng. Maskers";
                 break;
         }
+        return this.mensagem;
     }
 }
