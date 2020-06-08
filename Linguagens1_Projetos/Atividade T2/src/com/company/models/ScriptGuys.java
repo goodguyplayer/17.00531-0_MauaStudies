ac417a0e672fba018625a68990aaf193e856a171
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-06-08 12:16:55 -0300

    Created Scriptguys using Heavylifters as template
    Changing the message now

---
+++ Linguagens1_Projetos/Atividade T2/src/com/company/models/ScriptGuys.java
@@ -0,0 +1,22 @@
+package com.company.models;
+
+import com.company.enums.Horarios;
+import com.company.interfaces.PostarMensagem;
+
+public class ScriptGuys implements PostarMensagem {
+    private String mensagem;
+
+    @Override
+    public String postarMensagem(Horarios horarios) {
+
+        switch (horarios){
+            case REGULAR:
+                this.mensagem = "- Podem contar conosco!";
+                break;
+            case EXTRA:
+                this.mensagem =  "- N00b_qu3_n_Se_r3pita.bat";
+                break;
+        }
+        return this.mensagem;
+    }
+}
