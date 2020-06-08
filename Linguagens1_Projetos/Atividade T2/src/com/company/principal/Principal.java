75ecbeeda9cb0e8c8f4c726bc80552fe50b0db42
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-06-08 11:17:44 -0300

    Principal loop do main feito

--- Linguagens1_Projetos/Atividade T2/src/com/company/principal/Principal.java
+++ Linguagens1_Projetos/Atividade T2/src/com/company/principal/Principal.java
@@ -2,10 +2,10 @@ package com.company.principal;
 import java.util.Scanner;
 
 public class Principal {
-    //Deve realizar o cadastro, apresentação de novos membros, mandar alguem embora, trocar jornada de trabalho, postar mensagems, sair do sistema
+    // Deve realizar o cadastro, apresentação de novos membros, mandar alguem embora, trocar jornada de trabalho, postar mensagems, sair do sistema
 
-    //Segmento geral do menu
-    private String menu(){
+    // Segmento geral do menu
+    private String menu() {
         Scanner input = new Scanner(System.in);
         System.out.println("Bem vindo ao sistema MAsK_S0c13ty");
         System.out.println("Favor escolher uma das seguintes opções.:");
@@ -20,8 +20,37 @@ public class Principal {
         return escolha;
     }
 
-    public void run(){
-        menu();
-        
+    // O principal que roda tudo.
+    public void run() {
+        int exit = 0;
+        while (exit == 0) {
+            String escolha = menu();
+            switch (escolha) {
+                case "1":
+                    System.out.println("Favor inserir os dados.");
+                    break;
+                case "2":
+                    System.out.println("Todos os h4ck3rs cadastrados");
+                    break;
+                case "3":
+                    System.out.println("Escolha o h4ck3r");
+                    break;
+                case "4":
+                    System.out.println("Mudando horário");
+                case "5":
+                    System.out.println("Enviando mensagem geral. Relatório de mensagems enviadas.:");
+                    break;
+                case "6":
+                    System.out.println("Saindo...");
+                    exit += 1;
+                    break;
+                default:
+                    System.out.println("Input detectado não reconhecido. Favor tentar novamente.");
+                    break;
+            }
+        }
     }
 }
+
+
+
