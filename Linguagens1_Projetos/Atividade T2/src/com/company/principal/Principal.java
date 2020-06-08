87a21efc1ef5b09fd0dd679dcf8420cee60267ec
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-06-08 11:00:32 -0300

    Update principal com metodo menu

--- Linguagens1_Projetos/Atividade T2/src/com/company/principal/Principal.java
+++ Linguagens1_Projetos/Atividade T2/src/com/company/principal/Principal.java
@@ -1,4 +1,22 @@
 package com.company.principal;
 
 public class Principal {
+    //Deve realizar o cadastro, apresentação de novos membros, mandar alguem embora, trocar jornada de trabalho, postar mensagems, sair do sistema
+
+    //Segmento geral do menu
+    private int menu(){
+        System.out.println("Bem vindo ao sistema MAsK_S0c13ty");
+        System.out.println("Favor escolher uma das seguintes opções.:");
+        System.out.println("1 - Cadastrar novo h4ck3r");
+        System.out.println("2 - H4ck3rs cadastrados");
+        System.out.println("3 - Eliminar h4ck3r que fala muito");
+        System.out.println("4 - Mudar horario");
+        System.out.println("5 - Enviar mensagem para todos os h4ck3rs");
+        System.out.println("6 - Sair por hoje");
+        return 0;
+    }
+
+    public void run(){
+
+    }
 }
