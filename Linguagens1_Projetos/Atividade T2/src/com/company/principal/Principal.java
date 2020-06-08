6581a8f745a924f636114caecdd80914e0f9ad83
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-06-08 11:35:20 -0300

    Método cadastro feito em principal

--- Linguagens1_Projetos/Atividade T2/src/com/company/principal/Principal.java
+++ Linguagens1_Projetos/Atividade T2/src/com/company/principal/Principal.java
@@ -1,14 +1,20 @@
 package com.company.principal;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Principal {
     // Deve realizar o cadastro, apresentação de novos membros, mandar alguem embora, trocar jornada de trabalho, postar mensagems, sair do sistema
 
+    // Guardar todos os membros registrados
+    private ArrayList<String> membros = new ArrayList<String>();
+
+    // User input
+    Scanner input = new Scanner(System.in);
+
     // Segmento geral do menu
     private String menu() {
-        Scanner input = new Scanner(System.in);
         System.out.println("Bem vindo ao sistema MAsK_S0c13ty");
-        System.out.println("Favor escolher uma das seguintes opções.:");
+        System.out.println("Escolha uma das seguintes opções.:");
         System.out.println("1 - Cadastrar novo h4ck3r");
         System.out.println("2 - H4ck3rs cadastrados");
         System.out.println("3 - Eliminar h4ck3r que fala muito");
@@ -16,11 +22,31 @@ public class Principal {
         System.out.println("5 - Enviar mensagem para todos os h4ck3rs");
         System.out.println("6 - Sair por hoje");
 
-        String escolha = input.nextLine();
+        String escolha = this.input.nextLine();
         return escolha;
     }
 
-    //
+    // Método cadastro.: Recebe nome, email, enum tipo.
+    private String cadastro(){
+        // Username
+        System.out.println("Digitar o nome do h4ck3r.:");
+        String nome = this.input.nextLine();
+
+        // User email
+        System.out.println("Digitar o email do h4ck3r.:");
+
+        // User type
+        String email = this.input.nextLine();
+        System.out.println("Qual o tipo?");
+        System.out.println("1 - Mobile Members");
+        System.out.println("2 - Heavy Lifters");
+        System.out.println("3 - Script Guys");
+        System.out.println("4 - Big Brothers");
+        String type = this.input.nextLine();
+
+        return (nome + ";" +  email + ";" + type);
+    }
+    
 
     // O principal que roda tudo.
     public void run() {
@@ -30,22 +56,31 @@ public class Principal {
             switch (escolha) {
                 case "1":
                     System.out.println("Favor inserir os dados.");
+                    membros.add(cadastro());
+                    System.out.println("H4ck3r cadastrado com sucesso. Voltando ao menu...");
                     break;
+
                 case "2":
                     System.out.println("Todos os h4ck3rs cadastrados");
                     break;
+
                 case "3":
                     System.out.println("Escolha o h4ck3r");
                     break;
+
                 case "4":
                     System.out.println("Mudando horário");
+                    break;
+
                 case "5":
                     System.out.println("Enviando mensagem geral. Relatório de mensagems enviadas.:");
                     break;
+
                 case "6":
                     System.out.println("Saindo...");
                     exit += 1;
                     break;
+
                 default:
                     System.out.println("Input detectado não reconhecido. Favor tentar novamente.");
                     break;
