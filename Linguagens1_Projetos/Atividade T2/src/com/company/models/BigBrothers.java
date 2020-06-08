b89df371d729978ed34be3f5605e366b611a19b0
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-06-08 12:19:05 -0300

    Updated BigBrother's message
    ...

--- Linguagens1_Projetos/Atividade T2/src/com/company/models/BigBrothers.java
+++ Linguagens1_Projetos/Atividade T2/src/com/company/models/BigBrothers.java
@@ -1,4 +1,22 @@
 package com.company.models;
 
-public class BigBrothers {
+import com.company.enums.Horarios;
+import com.company.interfaces.PostarMensagem;
+
+public class BigBrothers implements PostarMensagem {
+    private String mensagem;
+
+    @Override
+    public String postarMensagem(Horarios horarios) {
+
+        switch (horarios){
+            case REGULAR:
+                this.mensagem = "- Sempre ajudando as pessoas membros ou não S2!";
+                break;
+            case EXTRA:
+                this.mensagem =  "- ...";
+                break;
+        }
+        return this.mensagem;
+    }
 }
