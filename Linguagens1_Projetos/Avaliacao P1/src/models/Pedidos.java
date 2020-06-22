97a13458986dd68f77f43ac2ea60e5c48142a9f9
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-06-22 18:03:33 -0300

    P1 - class Pedidos Atualizado
    - Criado metodo "criarPedido()"
    - Criado metodo "geradorId()" (Dado no exercicio)

--- Linguagens1_Projetos/Avaliacao P1/src/models/Pedidos.java
+++ Linguagens1_Projetos/Avaliacao P1/src/models/Pedidos.java
@@ -1,4 +1,24 @@
 package models;
 
+import enums.Pagamento;
+import enums.Status;
+
+import java.util.Random;
+
 public class Pedidos {
+    Pagamento pagamento;
+    Status status;
+
+    public String criarPedido(){
+
+        return "";
+    }
+
+    private String geradorId(){
+        Random random = new Random();
+        String idGerado = "";
+        for (int i = 0; i < 3; i++)
+            idGerado += random.nextInt(10);
+        return idGerado;
+    }
 }
