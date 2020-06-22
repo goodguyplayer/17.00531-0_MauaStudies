8c52a04a4fd5420923d343062e94f81360f438be
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-06-22 14:36:40 -0300

    P1 - Interface Autenticacao atualizado
    Adicionado.:
     - Boolean autenticado para auxiliar na autenticação do usuário.
    - Comentário base. Alterar depois.

--- Linguagens1_Projetos/Avaliacao P1/src/Interfaces/Autenticacao.java
+++ Linguagens1_Projetos/Avaliacao P1/src/Interfaces/Autenticacao.java
@@ -3,7 +3,9 @@ package Interfaces;
 /**
  * Auxilia na autenticação do usuário.
  *
+ * @param senha String usada para confirmar e permitir o uso de contas no sistema. No momento, há apenas uma conta.
  */
 
 public interface Autenticacao {
+    boolean autenticado(String senha);
 }
