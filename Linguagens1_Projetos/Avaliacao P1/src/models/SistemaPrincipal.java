13833cd541459f83b0f31df4284be756d30cadd8
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-06-22 19:58:26 -0300

    P1 - class SistemaPrincipal - metodo "options" atualizado
    Options facilita a visualização de quais são as opções para o sistema.

--- Linguagens1_Projetos/Avaliacao P1/src/models/SistemaPrincipal.java
+++ Linguagens1_Projetos/Avaliacao P1/src/models/SistemaPrincipal.java
@@ -14,6 +14,22 @@ public class SistemaPrincipal {
     Usuario usuario = new Usuario("Rato que ri","ratoqueri@gmail.com.br");
     ArrayList<String> pedidosfeitos = new ArrayList<String>();
 
+    public void run(){
 
+
+    }
+
+    /**
+     * Método options
+     * Feito para facilitar a adição de novas opções. Caso for necessário adicionar uma outra opção
+     * tipo "cancelar pedido", adicionar aqui a opção e mudar as escolhas em run().
+     */
+    private void options(){
+        System.out.println("Pizzaria - O rato que ri!");
+        System.out.println("1 - Nova venda");
+        System.out.println("2 - Verificar pedido");
+        System.out.println("3 - Alterar pedido");
+        System.out.println("0 - Nova venda");
+    }
 
 }
