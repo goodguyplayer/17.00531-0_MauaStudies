6e620e40113645f77568492c6a8a24f253767e79
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-06-22 15:18:12 -0300

    P1 - Primeiro javadocs criado - Teste para visualização

---
+++ Linguagens1_Projetos/Avaliacao P1/Javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"interfaces","l":"Autenticacao"},{"p":"<Unnamed>","l":"Main"},{"p":"enums","l":"Pagamento"},{"p":"enums","l":"Status"}]
