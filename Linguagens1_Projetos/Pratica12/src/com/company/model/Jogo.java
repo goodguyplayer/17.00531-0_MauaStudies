1702cbd2c85bd8dd5544acff0cda685083d816da
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-03 10:53:40 -0300

    Update Class Jogo - toString, setStatus, setStatusJogando

--- Linguagens1_Projetos/Pratica12/src/com/company/model/Jogo.java
+++ Linguagens1_Projetos/Pratica12/src/com/company/model/Jogo.java
@@ -6,11 +6,26 @@ public class Jogo {
     private String nome;
     private Status status;
 
-    public Jogo(String platform, String nome, Status status) {
+    public Jogo(String platform, String nome) {
         this.platform = platform;
         this.nome = nome;
+        this.status = Status.NAO_INICIADO;
+    }
+
+    @Override
+    public String toString() {
+        return "Jogo{" +
+                "platform='" + platform + '\'' +
+                ", nome='" + nome + '\'' +
+                ", status=" + status +
+                '}';
+    }
+
+    public void setStatus(Status status) {
         this.status = status;
     }
 
-    
+    public void setStatusJogando(){
+        this.status = Status.JOGANDO;
+    }
 }
