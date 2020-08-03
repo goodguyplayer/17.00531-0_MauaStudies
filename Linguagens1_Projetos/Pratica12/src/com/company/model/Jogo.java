d5ce078e8b6e01ac1a51445ce873432441a595e9
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-03 10:48:00 -0300

    Update Jogo.java

--- Linguagens1_Projetos/Pratica12/src/com/company/model/Jogo.java
+++ Linguagens1_Projetos/Pratica12/src/com/company/model/Jogo.java
@@ -1,7 +1,16 @@
 package com.company.model;
+import com.company.enums.Status;
 
 public class Jogo {
     private String platform;
     private String nome;
+    private Status status;
 
+    public Jogo(String platform, String nome, Status status) {
+        this.platform = platform;
+        this.nome = nome;
+        this.status = status;
+    }
+
+    
 }
