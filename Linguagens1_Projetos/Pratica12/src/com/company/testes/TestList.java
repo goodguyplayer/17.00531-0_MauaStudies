fc45c2e562c935aec7cbef6fd2037ec0c3318f7a
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-03 11:22:24 -0300

    Test ran. Enum works

--- Linguagens1_Projetos/Pratica12/src/com/company/testes/TestList.java
+++ Linguagens1_Projetos/Pratica12/src/com/company/testes/TestList.java
@@ -1,4 +1,20 @@
 package com.company.testes;
 
+import com.company.model.Jogo;
+import com.company.model.ListaDeJogos;
+
 public class TestList {
+    public static void main(String[] args) {
+        ListaDeJogos jogos = new ListaDeJogos();
+
+        Jogo one = new Jogo("PC", "Halo");
+        Jogo two = new Jogo("PC", "Diablo");
+        Jogo three = new Jogo("PC", "Halo");
+
+        jogos.addJogo(one);
+        jogos.addJogo(two);
+        jogos.addJogo(three);
+
+        System.out.println("Game list.: " + jogos.toString());
+    }
 }
