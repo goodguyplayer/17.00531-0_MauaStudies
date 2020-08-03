4c6b83fc4071a445e285b8bb3729d9f9b98f5837
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-03 11:00:21 -0300

    Created test file

---
+++ Linguagens1_Projetos/Pratica12/src/com/company/testes/TestJogo.java
@@ -0,0 +1,10 @@
+package com.company.testes;
+
+import com.company.model.Jogo;
+
+public class TestJogo {
+    public static void main(String[] args) {
+        Jogo jogo = new Jogo("PC", "Diablo");
+        System.out.println(jogo);
+    }
+}
