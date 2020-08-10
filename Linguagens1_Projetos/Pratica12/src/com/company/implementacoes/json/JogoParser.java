0039bba650cc83f2c821d8c47c43e843d114a8c3
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-10 10:49:59 -0300

    Updated StatusParser

--- Linguagens1_Projetos/Pratica12/src/com/company/implementacoes/json/JogoParser.java
+++ Linguagens1_Projetos/Pratica12/src/com/company/implementacoes/json/JogoParser.java
@@ -1,23 +1,24 @@
 package com.company.implementacoes.json;
 
 import com.company.model.Jogo;
+import com.company.parser.StatusParser;
 import org.json.JSONObject;
 
 public class JogoParser {
     public static JSONObject toJson(Jogo jogo){
             JSONObject json = new JSONObject();
             json.put("platform", jogo.getPlatform());
-            json.put("name", jogo.getNome());
+            json.put("nome", jogo.getNome());
             json.put("status", jogo.getStatus());
             return json;
     }
 
     public static Jogo fromJson(JSONObject json){
         Jogo jogo = new Jogo(
-                json.getString("plataforma"),
+                json.getString("platform"),
                 json.getString("nome")
         );
-        jogo.setStatus(json.getString("status"));
+        jogo.setStatus(StatusParser.toStatus(json.getString("status")));
         return jogo;
     }
 }
--- Linguagens1_Projetos/Pratica12/src/com/company/testes/TesteJSon.java
+++ Linguagens1_Projetos/Pratica12/src/com/company/testes/TesteJSon.java
@@ -1,5 +1,6 @@
 package com.company.testes;
 
+import com.company.enums.Status;
 import com.company.implementacoes.json.JogoParser;
 import com.company.model.Jogo;
 import org.json.JSONObject;
@@ -8,9 +9,12 @@ public class TesteJSon {
     public static void main(String[] args) {
         Jogo jogo1 = new Jogo("PC", "Cyberpunk");
         JSONObject json = JogoParser.toJson(jogo1);
+        jogo1.setStatus(Status.TERMINADO);
         System.out.println("Formato toString:" + jogo1.toString());
         System.out.println("-----------------");
         System.out.println("Formato json:" + json);
+        Jogo jogo2 = JogoParser.fromJson(json);
+        System.out.println("Jogo 2:" + jogo2);
     }
 
 
