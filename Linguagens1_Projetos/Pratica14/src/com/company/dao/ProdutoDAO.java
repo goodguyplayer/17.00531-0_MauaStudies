245b7e610f53c0e04a589543f1843bb020138114
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-24 11:54:43 -0300

    Update ProdutoDAO.java

--- Linguagens1_Projetos/Pratica14/src/com/company/dao/ProdutoDAO.java
+++ Linguagens1_Projetos/Pratica14/src/com/company/dao/ProdutoDAO.java
@@ -2,12 +2,47 @@ package com.company.dao;
 
 import com.company.models.Produto;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProdutoDAO implements DAO<Produto> {
+    private Connection connection;
+    private String myDBConnectionString = "jdbc:sqlite:dados.db";
+
+    public ProdutoDAO (){
+        try {
+            connection = DriverManager.getConnection(myDBConnectionString);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+
     @Override
     public List<Produto> getAll() {
-        return null;
+        List<Produto> produtos = new ArrayList<>();
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM produtos");
+            while(result.next()){
+                Produto produto = new Produto(
+                        result.getString("codigo"),
+                        result.getString("nome"),
+                        result.getString("descricao"),
+                        result.getDouble("custo"),
+                        result.getDouble("valor"),
+                        result.getInt("quantidade")
+                );
+                produtos.add(produto);
+            }
+            result.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return produtos;
     }
 
     @Override
