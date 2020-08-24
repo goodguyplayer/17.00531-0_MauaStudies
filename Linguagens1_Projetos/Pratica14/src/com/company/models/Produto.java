19be56ce0edb64d4dcfdaebf31b39ba491a88501
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-24 10:46:58 -0300

    Added vars, constructor and toString

--- Linguagens1_Projetos/Pratica14/src/com/company/models/Produto.java
+++ Linguagens1_Projetos/Pratica14/src/com/company/models/Produto.java
@@ -1,4 +1,31 @@
 package com.company.models;
 
 public class Produto {
+    private String codigo;
+    private String nome;
+    private String descricao;
+    private double custo;
+    private double valor;
+    private int quantidade;
+
+    public Produto(String codigo, String nome, String descricao, double custo, double valor, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.custo = custo;
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo='" + codigo + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", custo=" + custo +
+                ", valor=" + valor +
+                ", quantidade=" + quantidade +
+                '}';
+    }
 }
