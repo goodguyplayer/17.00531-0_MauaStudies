f07b3f7c3ec78e3bca76bb25c5a228270140f298
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-24 11:47:01 -0300

    Update DAO.java

--- Linguagens1_Projetos/Pratica14/src/com/company/dao/DAO.java
+++ Linguagens1_Projetos/Pratica14/src/com/company/dao/DAO.java
@@ -1,5 +1,12 @@
 package com.company.dao;
 
+import java.util.List;
+
 public interface DAO <T>{
+    List<T> getAll();
+
+    void update(T t);
+    void delete(T t);
+    void create(T t);
     
 }
