48c4d4542de701cca5308503f8cc043e8d44a499
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-24 11:48:48 -0300

    Update ProdutoDAO.java

--- Linguagens1_Projetos/Pratica14/src/com/company/dao/ProdutoDAO.java
+++ Linguagens1_Projetos/Pratica14/src/com/company/dao/ProdutoDAO.java
@@ -1,4 +1,27 @@
 package com.company.dao;
 
-public class ProdutoDAO {
+import com.company.models.Produto;
+
+import java.util.List;
+
+public class ProdutoDAO implements DAO<Produto> {
+    @Override
+    public List<Produto> getAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Produto produto) {
+
+    }
+
+    @Override
+    public void delete(Produto produto) {
+
+    }
+
+    @Override
+    public void create(Produto produto) {
+
+    }
 }
