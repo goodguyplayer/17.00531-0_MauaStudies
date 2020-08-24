0eb23f148077cdce84dcc3f7ee5eba2487a6bba8
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-24 10:52:53 -0300

    Added statements and executeQuery

--- Linguagens1_Projetos/Pratica14/src/com/company/testes/Aplicacao.java
+++ Linguagens1_Projetos/Pratica14/src/com/company/testes/Aplicacao.java
@@ -2,12 +2,16 @@ package com.company.testes;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class Aplicacao {
     public void run(){
         try {
             Connection connection = DriverManager
                     .getConnection("jdbc:sqlite:dados.db");
+            Statement statement = connection.createStatement();
+            statement.executeQuery("SELECT * FROM produtos");
+            
         } catch (Exception exception){
             exception.printStackTrace();
         }
