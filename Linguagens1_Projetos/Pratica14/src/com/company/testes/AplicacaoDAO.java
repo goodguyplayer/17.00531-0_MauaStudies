b829dcd415165347343ed621c445f0872621adf8
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-24 12:13:32 -0300

    Update AplicacaoDAO.java

--- Linguagens1_Projetos/Pratica14/src/com/company/testes/AplicacaoDAO.java
+++ Linguagens1_Projetos/Pratica14/src/com/company/testes/AplicacaoDAO.java
@@ -3,13 +3,23 @@ package com.company.testes;
 import com.company.dao.ProdutoDAO;
 import com.company.models.Produto;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AplicacaoDAO {
+    private List<Produto> produtos;
+    private ProdutoDAO produtoDAO;
+    private Scanner scanner;
+
+    public AplicacaoDAO() {
+        produtos = new ArrayList<>();
+        produtoDAO = new ProdutoDAO();
+        scanner = new Scanner(System.in);
+
+    }
+
     public void run(){
-        List<Produto> produtos;
-        ProdutoDAO produtoDAO = new ProdutoDAO();
-        produtos = produtoDAO.getAll();
-        produtos.forEach(produto -> System.out.println(produto));
+
     }
 }
