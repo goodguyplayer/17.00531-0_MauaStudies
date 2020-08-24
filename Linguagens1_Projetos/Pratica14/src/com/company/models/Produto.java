6eecf3e9439ef15ca5650580ad28993dba15fd70
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-24 11:27:06 -0300

    Added getters

--- Linguagens1_Projetos/Pratica14/src/com/company/models/Produto.java
+++ Linguagens1_Projetos/Pratica14/src/com/company/models/Produto.java
@@ -17,6 +17,30 @@ public class Produto {
         this.quantidade = quantidade;
     }
 
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getCusto() {
+        return custo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
     @Override
     public String toString() {
         return "Produto{" +
