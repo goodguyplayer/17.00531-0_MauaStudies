9d38a1e4c05b6ea7e18c994529edde4be6526821
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-24 11:56:59 -0300

    Updated and ran code

--- Linguagens1_Projetos/Pratica14/src/com/company/Main.java
+++ Linguagens1_Projetos/Pratica14/src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
 import com.company.testes.Aplicacao;
+import com.company.testes.AplicacaoDAO;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Aplicacao app = new Aplicacao();
+        AplicacaoDAO app = new AplicacaoDAO();
         app.run();
 
     }
--- Linguagens1_Projetos/Pratica14/src/com/company/testes/AplicacaoDAO.java
+++ Linguagens1_Projetos/Pratica14/src/com/company/testes/AplicacaoDAO.java
@@ -1,4 +1,15 @@
 package com.company.testes;
 
+import com.company.dao.ProdutoDAO;
+import com.company.models.Produto;
+
+import java.util.List;
+
 public class AplicacaoDAO {
+    public void run(){
+        List<Produto> produtos;
+        ProdutoDAO produtoDAO = new ProdutoDAO();
+        produtos = produtoDAO.getAll();
+        produtos.forEach(produto -> System.out.println(produto));
+    }
 }
