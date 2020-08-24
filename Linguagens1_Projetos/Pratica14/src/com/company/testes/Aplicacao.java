312c3ec7ea87278409a8f05272fadf8420c9aa04
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-24 11:38:14 -0300

    Updated and got it to run

--- Linguagens1_Projetos/Pratica14/src/com/company/testes/Aplicacao.java
+++ Linguagens1_Projetos/Pratica14/src/com/company/testes/Aplicacao.java
@@ -30,18 +30,35 @@ public class Aplicacao {
             Produto produto = new Produto("2000", "Water cooler 120mm", "Cooler", 80, 240, 5);
             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO produtos (codigo, nome, descricao, custo, valor, quantidade) VALUES (?, ?, ?, ?, ?, ?);" );
             preparedStatement.setString(1, produto.getCodigo());
-            preparedStatement.setString(1, produto.getNome());
-            preparedStatement.setString(1, produto.getDescricao());
-            preparedStatement.setDouble(1, produto.getCusto());
-            preparedStatement.setDouble(1, produto.getValor());
-            preparedStatement.setInt(1, produto.getQuantidade());
+            preparedStatement.setString(2, produto.getNome());
+            preparedStatement.setString(3, produto.getDescricao());
+            preparedStatement.setDouble(4, produto.getCusto());
+            preparedStatement.setDouble(5, produto.getValor());
+            preparedStatement.setInt(6, produto.getQuantidade());
 
             // Execute prepared statement
             int retorno = preparedStatement.executeUpdate();
             System.out.println("Retorno: " + retorno);
 
             // Save modifications
-            connection.commit();
+            //connection.commit();
+
+            // Exibe conteudo (Bad method)
+            //statement.executeQuery("SELECT * FROM produtos");
+
+            while (result.next()){
+                Produto newproduto = new Produto(
+                        result.getString("codigo"),
+                        result.getString("nome"),
+                        result.getString("descricao"),
+                        result.getDouble("custo"),
+                        result.getDouble("valor"),
+                        result.getInt("quantidade")
+                );
+                System.out.println(newproduto);
+            }
+
+            result.close();
 
 
         } catch (Exception exception){
