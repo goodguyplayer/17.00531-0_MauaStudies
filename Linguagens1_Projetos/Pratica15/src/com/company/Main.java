dec15b2a2f8ec6af2ae9395a5ee1b213e9c44ec7
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-14 10:39:00 -0300

    A15 - updated LeituraJavaAte8

--- Linguagens1_Projetos/Pratica15/src/com/company/Main.java
+++ Linguagens1_Projetos/Pratica15/src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
@@ -7,6 +12,26 @@ public class Main {
 
     }
 
-    private static void LeituraJavaAte8() {
+    private static void LeituraJavaAte8() throws Exception {
+        URL url = new URL("https://viacep.com.br/ws/01001000/json/");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        int statusCode = con.getResponseCode();
+        System.out.println("Status code.: " + statusCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        con.getInputStream()
+                )
+        );
+
+        String result;
+        StringBuffer content = new StringBuffer();
+        while( (result = in.readLine()) != null){
+            content.append(result);
+        }
+        in.close();
+        System.out.println("Recebidos:");
+        System.out.println(content);
     }
 }
