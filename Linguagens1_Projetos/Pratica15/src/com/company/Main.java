94c9376e01433acec063aa716b437cf98d2b2d22
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-14 12:05:37 -0300

    A15 - Main.java class

--- Linguagens1_Projetos/Pratica15/src/com/company/Main.java
+++ Linguagens1_Projetos/Pratica15/src/com/company/Main.java
@@ -3,12 +3,34 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-	    LeituraJavaAte8();
+	    //LeituraJavaAte8();
+        LeituraJava11();
+        
+
+    }
+
+    private static void LeituraJava11() throws Exception{
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("https://api.magicthegathering.io/v1/cards?name=anaba%20bodyguard"))
+                .build();
+
+        HttpResponse<String> response = client.send(
+                request, HttpResponse.BodyHandlers.ofString()
+        );
+        System.out.println("Status Code:" + response.statusCode());
+        System.out.println("Recebidos:");
+        System.out.println(response.body());
 
     }
 
