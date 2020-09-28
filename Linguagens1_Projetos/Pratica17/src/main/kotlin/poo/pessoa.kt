a198dcbba3361ebd22b8b2e417f1701f9b7186a4
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-28 11:07:10 -0300

    Pratica17 - Class Pessoa - Added toString

--- Linguagens1_Projetos/Pratica17/src/main/kotlin/poo/pessoa.kt
+++ Linguagens1_Projetos/Pratica17/src/main/kotlin/poo/pessoa.kt
@@ -16,5 +16,9 @@ class Pessoa(nome: String, cpf: String = "", email: String = ""){
         this.email = email
     }
 
+    override fun toString(): String {
+        return "Pessoa(nome='$nome', cpf='$cpf', email='$email')"
+    }
+
 
 }
