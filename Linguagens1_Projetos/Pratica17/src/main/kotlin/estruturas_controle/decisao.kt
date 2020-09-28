98fbc228776ce9520888a75ebe28b9f05a89715e
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-28 10:30:33 -0300

    Pratica17 - decisao.kt updated

--- Linguagens1_Projetos/Pratica17/src/main/kotlin/estruturas_controle/decisao.kt
+++ Linguagens1_Projetos/Pratica17/src/main/kotlin/estruturas_controle/decisao.kt
@@ -12,4 +12,12 @@ fun main() {
     val numero2 = 40
     val resultado = if (numero2 % 2 == 0) "Even" else "Odd"
     println("${resultado}")
+
+    // Elvis operator
+    // Explicitly says the var can receive a null value
+    var nome : String ?
+    nome = "Miguel"
+    println(nome)
+    nome = null
+    println("${nome ?: "Ryu" }")
 }
