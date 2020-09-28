10268bbb50835107256d2ee52609279c64e6bddd
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-28 10:07:00 -0300

    Pratica17 - Variaveis.kt updated - added Double

--- Linguagens1_Projetos/Pratica17/src/main/kotlin/basico/variaveis.kt
+++ Linguagens1_Projetos/Pratica17/src/main/kotlin/basico/variaveis.kt
@@ -6,5 +6,9 @@ fun main() {
     var numero_2 = 45.87
     println(nome)
 //    nome = 13  -- Não pode acontecer pois kotlin é fortemente tipado
+    var resultado : Double
+    resultado = numero_1 + numero_2
+    println(resultado)
+    println("O valor do resultado: $resultado") //String template
 
 }
