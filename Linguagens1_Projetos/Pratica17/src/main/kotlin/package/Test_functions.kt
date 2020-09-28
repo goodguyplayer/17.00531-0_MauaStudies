b2f198091f2fdf502fb0309ee742f7d89f294908
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-28 11:27:54 -0300

    Pratica17 - Test_functions.kt updated

--- Linguagens1_Projetos/Pratica17/src/main/kotlin/package/Test_functions.kt
+++ Linguagens1_Projetos/Pratica17/src/main/kotlin/package/Test_functions.kt
@@ -1,2 +1,14 @@
 package `package`
 
+//Declarar uma funcao
+fun SumTwoNumbers(numero1: Int, numero2: Int) : Int{
+    return numero1 + numero2
+}
+
+// methods of one instruction
+fun MultiplyTwoNumbers(numero1: Int, numero2: Int) = numero1*numero2
+
+fun main() {
+    println("${SumTwoNumbers(36, 6)}")
+    println("${MultiplyTwoNumbers(36, 6)}")
+}
