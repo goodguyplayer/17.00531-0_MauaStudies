1274171c3ac5db5debbea8f693230818d2b766f3
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-28 10:49:03 -0300

    Pratica17 - colecoes_geral.kt updated

--- Linguagens1_Projetos/Pratica17/src/main/kotlin/colecoes/colecoes_geral.kt
+++ Linguagens1_Projetos/Pratica17/src/main/kotlin/colecoes/colecoes_geral.kt
@@ -1,2 +1,13 @@
 package colecoes
 
+fun main() {
+    val meuVetor = arrayOf(1,2,3)
+    var item : Int
+    for (item in meuVetor){
+        println(item)
+    }
+    println("Tamanho array.: ${meuVetor.size}")
+    println("Primeiro elemento.: ${meuVetor[0]}")
+    println("Maior valor.: ${meuVetor.max()}")
+
+}
