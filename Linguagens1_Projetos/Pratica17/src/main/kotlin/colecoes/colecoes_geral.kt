97abfc3e8f8d4594c81075564cf00285a5b9a25c
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-28 10:53:32 -0300

    Pratica17 - colecoes_geral.kt updated

--- Linguagens1_Projetos/Pratica17/src/main/kotlin/colecoes/colecoes_geral.kt
+++ Linguagens1_Projetos/Pratica17/src/main/kotlin/colecoes/colecoes_geral.kt
@@ -1,6 +1,7 @@
 package colecoes
 
 fun main() {
+    // Array
     val meuVetor = arrayOf(1,2,3)
     var item : Int
     for (item in meuVetor){
@@ -10,4 +11,14 @@ fun main() {
     println("Primeiro elemento.: ${meuVetor[0]}")
     println("Maior valor.: ${meuVetor.max()}")
 
+    // List
+    // Object list non mutable
+    val minhaLista = listOf("Hi", "Hello", "Eyo")
+    println(minhaLista)
+
+    // mutable list
+    val minhaListMutable = mutableListOf("Hi", "Hello", "Eyo")
+    println(minhaListMutable)
+    minhaListMutable.add("Goku")
+    println(minhaListMutable)
 }
