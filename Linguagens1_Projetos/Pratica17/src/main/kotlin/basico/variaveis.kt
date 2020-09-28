522ba33e36287f5dd363118894109d7d3cb4ecd9
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-28 10:05:07 -0300

    Pratica17 - Variaveis.kt updated - Added some vars

--- Linguagens1_Projetos/Pratica17/src/main/kotlin/basico/variaveis.kt
+++ Linguagens1_Projetos/Pratica17/src/main/kotlin/basico/variaveis.kt
@@ -1,2 +1,10 @@
 package basico
 
+fun main() {
+    var nome = "Johnny Lawrence"
+    var numero_1 = 10
+    var numero_2 = 45.87
+    println(nome)
+//    nome = 13  -- Não pode acontecer pois kotlin é fortemente tipado
+
+}
