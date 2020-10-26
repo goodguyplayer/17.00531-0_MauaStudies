c8d083f7b67cec1f8e0061e42f4d04188f08fe6c
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-26 10:17:22 -0300

    Pratica18 - entrada_de_dados2.kt created

---
+++ Linguagens1_Projetos/Pratica18/src/main/kotlin/Fundamentos/entrada_de_dados2.kt
@@ -0,0 +1,8 @@
+package Fundamentos
+
+fun main() {
+    print("Please input a name.: ")
+    var nome = readLine();
+    println("Inputted name.: $nome")
+    println("Size of name.: ${nome?.length}")
+}
