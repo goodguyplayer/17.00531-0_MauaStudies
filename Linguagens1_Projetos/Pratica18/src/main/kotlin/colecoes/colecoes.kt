21e6d25dd5c2982b61d121a16be19cccfd3983ec
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-26 12:23:29 -0300

    Pratica18 - colecoes.kt created

---
+++ Linguagens1_Projetos/Pratica18/src/main/kotlin/colecoes/colecoes.kt
@@ -0,0 +1,12 @@
+package colecoes
+
+fun main() {
+    val minhaLista1 = listOf(3, 29, 3, 5, 6)
+    minhaLista1.forEach{ println(it)}
+
+    val minhaLista2 = mutableListOf(3, 29, 3, 5, 6)
+    minhaLista2.forEach{ println(it)}
+    minhaLista2.add(10)
+    minhaLista2.add(-10)
+    minhaLista2.forEach{ println(it)}
+}
