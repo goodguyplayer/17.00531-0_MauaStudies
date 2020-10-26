e0566405ad1193bb19c9139a3896a2d4af875ae8
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-26 11:21:16 -0300

    Pratica18 - Pushing everything

--- Linguagens1_Projetos/Pratica18/src/main/kotlin/Estruturas/funcoes.kt
+++ Linguagens1_Projetos/Pratica18/src/main/kotlin/Estruturas/funcoes.kt
@@ -1,7 +1,9 @@
 package Estruturas
 
 fun somar(num1:Int, num2:Int) = num1+num2
+fun somar2(num1:Int, num2:Int = 6) = num1+num2
 
 fun main() {
-    
+    println(somar(5,11))
+    println(somar2(6))
 }
