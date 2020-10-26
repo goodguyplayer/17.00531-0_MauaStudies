c2119f8243650796eecfd41a41a235f8dc5ff863
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-26 11:46:11 -0300

    Pratica18 - tudojunto.kt updated

--- Linguagens1_Projetos/Pratica18/src/main/kotlin/oo/tudojunto.kt
+++ Linguagens1_Projetos/Pratica18/src/main/kotlin/oo/tudojunto.kt
@@ -1,2 +1,15 @@
 package oo
 
+class Animal(val nome:String, var peso:Double){
+    override fun toString(): String {
+        return "Animal(nome='$nome', peso=$peso)"
+    }
+}
+
+fun main() {
+    val animal1 = Animal("Fido", 12.21)
+    val animal2 = Animal("Thor", 42.24)
+
+    println(animal1)
+    println(animal2)
+}
