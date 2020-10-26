8fbad7e3432d70ba591862609f990af77c1bb213
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-10-26 10:27:37 -0300

    Pratica18 - conversao_de_tipo.kt updated

--- Linguagens1_Projetos/Pratica18/src/main/kotlin/Fundamentos/conversao_de_tipo.kt
+++ Linguagens1_Projetos/Pratica18/src/main/kotlin/Fundamentos/conversao_de_tipo.kt
@@ -1,2 +1,19 @@
 package Fundamentos
 
+fun main() {
+    var num1 : Double
+    var num2 : Int
+    var Result : Double
+
+    println("Please input two numbers")
+    print("Real number.: ")
+    num1 = readLine()!!.toDouble()
+
+    print("Int number.: ")
+    num2 = readLine()!!.toInt()
+
+    Result = num1 + num2
+    println("Result.: $Result")
+    println("Result.: ${num1 + num2}")
+
+}
